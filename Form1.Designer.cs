96acc6816622632cbe2ea038e875f58cb383a1c2
diego <136401880+DKB-17@users.noreply.github.com>
2024-08-13 20:46:36 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Opengl2d
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl5 = new OpenTK.GLControl();
+            this.glControl6 = new OpenTK.GLControl();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // glControl5
+            // 
+            this.glControl5.BackColor = System.Drawing.Color.Black;
+            this.glControl5.Location = new System.Drawing.Point(12, 22);
+            this.glControl5.Name = "glControl5";
+            this.glControl5.Size = new System.Drawing.Size(330, 330);
+            this.glControl5.TabIndex = 0;
+            this.glControl5.VSync = false;
+            // 
+            // glControl6
+            // 
+            this.glControl6.BackColor = System.Drawing.Color.Black;
+            this.glControl6.Location = new System.Drawing.Point(346, 22);
+            this.glControl6.Name = "glControl6";
+            this.glControl6.Size = new System.Drawing.Size(330, 330);
+            this.glControl6.TabIndex = 1;
+            this.glControl6.VSync = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(682, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Lados";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(685, 53);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(69, 20);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(685, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Desenhar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(682, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Raio";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(770, 355);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.glControl6);
+            this.Controls.Add(this.glControl5);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private OpenTK.GLControl glControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private OpenTK.GLControl glControl2;
+        private OpenTK.GLControl glControl3;
+        private OpenTK.GLControl glControl4;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private OpenTK.GLControl glControl5;
+        private OpenTK.GLControl glControl6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Opengl2d
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void drawPoligono(int lados, bool prencimento) 
+        {
+
+            int radius = 80;
+            float n = 360 / lados;
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Color3(Color.White);
+            if (prencimento)
+            {
+                GL.Begin(PrimitiveType.Triangles);
+            }
+            else
+            {
+                GL.Begin(PrimitiveType.LineLoop);
+            }
+            for (float i = 0; i < 360; i += n) {
+                double degInRad = i * (3.1416 / 180);
+                GL.Color3(Color.Red);
+                GL.Vertex2(Math.Cos(degInRad) * radius, Math.Sin(degInRad) * radius);
+                GL.Color3(Color.Blue);
+                GL.Vertex2(0, 0);
+                if (!prencimento)
+                {
+                    GL.Color3(Color.Red);
+                    GL.Vertex2(Math.Cos(degInRad) * radius, Math.Sin(degInRad) * radius);
+                }
+                float d = i + n;
+                degInRad = d * (3.1416 / 180);
+                GL.Color3(Color.Green);
+                GL.Vertex2(Math.Cos(degInRad) * radius, Math.Sin(degInRad) * radius);
+                if (!prencimento)
+                {
+                    GL.Color3(Color.Green);
+                    GL.Vertex2(Math.Cos(degInRad) * radius, Math.Sin(degInRad) * radius);
+                    GL.Color3(Color.Blue);
+                    GL.Vertex2(0, 0);
+                }
+            }
+            GL.End();
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            int lados = 180;
+            try
+            {
+                int.TryParse(numericUpDown2.Value.ToString(), out lados);
+            }
+            catch { MessageBox.Show("Não deu certo"); }
+
+            glControl5.MakeCurrent();
+            GL.LoadIdentity();
+            GL.Ortho(-100, 100, -100, 100, -1, 1);
+            GL.ClearColor(Color.Black);
+
+            drawPoligono(lados, true);
+            glControl5.SwapBuffers();
+
+            glControl6.MakeCurrent();
+            GL.LoadIdentity();
+            GL.Ortho(-100, 100, -100, 100, -1, 1);
+            GL.ClearColor(Color.Black);
+
+            drawPoligono(lados, false);
+            glControl6.SwapBuffers();
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
